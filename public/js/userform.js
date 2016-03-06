324b2ec2fe495a956b3870f163a05282680c58a0
John <johnclarkthiel@Johns-MBP.home>
2016-03-06 18:14:24 -0500

    adding root scope to log in auth, adding resolve to config when user logged in

--- public/js/userform.js
+++ public/js/userform.js
@@ -1,13 +1,16 @@
 var app = angular.module('user-form', ['ngRoute']);
 
-app.controller('FormController',['$http', '$scope', function($http,$scope){
+app.controller('FormController',['$http', '$scope', '$location', '$rootScope', function($http,$scope, $location, $rootScope){ //location changes the hash values in the url
 	var controller = this;
 	this.name = null;
 	this.password = null;
 
 
-	this.addUser = function() {
+	$scope.addUser = function() {
 		console.log('WORKING');
+		this.signupName = $scope.signupName;
+		this.signupPassword = $scope.signupPassword;
+
 		$http({
 			method: 'POST',
 			url: '/users/signup',
@@ -21,23 +24,33 @@ app.controller('FormController',['$http', '$scope', function($http,$scope){
 		function(err){
 			console.log(err);
 		});
+		$location.path('/user'); //will change the URL hash value to to /root/user ... same as window.location.hash = '#/user' ... no hash needed, b/c 'path' automatically knows we're working with angular	
 	};
 
-	this.loginUser = function() {
+	$scope.loginUser = function() {
 		console.log('WORKING!!!');
-		$http({
-			method: 'GET',
-			url: '/users/login',
-			data: this
-		}).then(function(response){
-			console.log(response.data);
-			console.log($scope);
-		}, 
-		function(err){
-			console.log(err);
-		});
+		//run an http get here and see if it matches any user name or passwords?
+	//$rooteScope is a super global variable ... attaches all of your properties to a global object
+		if ($scope.username == 'John' && $scope.password == 'John') {
+			$rootScope.loggedIn = true;
+			$location.path('/user'); 	
+			$http({
+				method: 'GET',
+				url: '/users/login',
+				data: this
+			}).then(function(response){
+				console.log(response.data);
+				console.log($scope);
+			}, 
+			function(err){
+				console.log(err);
+			});
+		} else {
+		alert('You are not signed up');
+		}
 	};
-}]);
+
+}]); //<<<<<<END Form Controller
 
 app.directive('userForm', function(){
 	return {
@@ -56,6 +69,16 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 		controller: 'FormController', 
 		controllerAs: 'formCtrl'
 	}).when('/user', {
+		resolve: {
+			'check' : function($location, $rootScope) {
+				console.log('RESOLVE WORKING');
+				if ($rootScope.loggedIn) {
+					$location.path('/user');
+				} else {
+					$location.path('/');
+				}
+			}
+		},
 		templateUrl: 'partials/fakeuserpage.html',
 		controller: 'FormController', 
 		controllerAs: 'formCtrl'
