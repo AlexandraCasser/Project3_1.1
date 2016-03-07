7fd9be3b078d4184b41180f4d5d4194558618e0a
John <johnclarkthiel@Johns-MBP.home>
2016-03-06 19:42:36 -0500

    updating login user function with http post call

--- public/js/userform.js
+++ public/js/userform.js
@@ -5,15 +5,16 @@ app.controller('FormController',['$http', '$scope', '$location', '$rootScope', f
 	this.name = null;
 	this.password = null;
 
-
 	$scope.addUser = function() {
 		console.log('WORKING');
 		this.signupName = $scope.signupName;
 		this.signupPassword = $scope.signupPassword;
+		console.log(this.signupName);
+		console.log(this.signupPassword);
 
 		$http({
 			method: 'POST',
-			url: '/users/signup',
+			url: '/user/',
 			data: this
 		}).then(function(response){
 			console.log(resonse.data);
@@ -24,19 +25,34 @@ app.controller('FormController',['$http', '$scope', '$location', '$rootScope', f
 		function(err){
 			console.log(err);
 		});
+
 		$location.path('/user'); //will change the URL hash value to to /root/user ... same as window.location.hash = '#/user' ... no hash needed, b/c 'path' automatically knows we're working with angular	
 	};
 
 	$scope.loginUser = function() {
 		console.log('WORKING!!!');
-		//run an http get here and see if it matches any user name or passwords?
+	//need to run an ajax POST call to authenticate user name and password and have the server authenticate then
 	//$rooteScope is a super global variable ... attaches all of your properties to a global object
-		if ($scope.username == 'John' && $scope.password == 'John') {
+		this.username = $scope.username;
+		this.password = $scope.password;
+		console.log('NAME ' + this.username + ' PASSWORD ' +  this.password);
+		// $http({
+		// 	method: 'POST',
+		// 	url: '/user/',
+		// 	data: this
+		// }).then(function(response){
+		// 	console.log(response.data); //looking for req.user.id here? We need the server to auth and then we need to grab this somehow
+		// },
+		// function(err){
+		// 	console.log(err);
+		// });
+
+		if ($scope.username == 'John' && $scope.password == 'John') { //need to change condition to if authenticated
 			$rootScope.loggedIn = true;
-			$location.path('/user'); 	
+			$location.path('/user/'); 	
 			$http({
 				method: 'GET',
-				url: '/users/login',
+				url: '/user/' + req.user.id, //<<<<< or some sort of id ... 
 				data: this
 			}).then(function(response){
 				console.log(response.data);
@@ -59,7 +75,7 @@ app.directive('userForm', function(){
 		controller: 'FormController',
 		controllerAs: 'formCtrl'
 	}
-});
+}); //<<<<<< END form directive
 
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
