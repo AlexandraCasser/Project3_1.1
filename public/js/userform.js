8d770e97fb960b95cb684b419cb19faa24062e5f
John <johnclarkthiel@Johns-MacBook-Pro.local>
2016-03-07 13:39:37 -0500

    updating

--- public/js/userform.js
+++ public/js/userform.js
@@ -17,23 +17,16 @@ app.controller('FormController',['$http', '$scope', '$location', '$rootScope', '
 
 		$http.post('/user/signup', {username : uname, password : pword}).then(function(response){
 
-			// innerController.userID = response.data._id;
+			var userID = response.data._id;
 
 
 			// console.log(controller)
 			//This will now post to the user ID
 			$location.path('/user/' + response.data._id); //will change the URL hash value to to /root/user ... same as window.location.hash = '#/user' ... no hash needed, b/c 'path' automatically knows we're working with angular	
 			
-			//emit the response.data._id to MainController
-			$scope.emit("userInfo", response.data._id)
-						console.log("THIS IS THE USERID", response.data._id);
-			 $http.get("/user/" + response.data._id);
-        .then(function(response){
-            console.log("This is the get /users/:id response", response)
-            controller.name = response;
+			 $http.get("/user/" + userID).then(function(response){
+            console.log(response);
         })
-    }
-
 		},
 		function(err){
 			alert('ERROR');
@@ -59,23 +52,23 @@ app.controller('FormController',['$http', '$scope', '$location', '$rootScope', '
 			console.log(err);
 		});
 
-		if ($scope.username == 'John' && $scope.password == 'John') { //need to change condition to if authenticated
-			$rootScope.loggedIn = true;
-			$location.path('/user/login'); 	
-			$http({
-				method: 'GET',
-				url: '/user/' + req.user.id, //or some sort of id ... 
-				data: this
-			}).then(function(response){
-				console.log(response.data);
-				console.log($scope);
-			}, 
-			function(err){
-				console.log(err);
-			});
-		} else {
-		alert('You are not signed up');
-		}
+		// if ($scope.username == 'John' && $scope.password == 'John') { //need to change condition to if authenticated
+		// 	$rootScope.loggedIn = true;
+		// 	$location.path('/user/login'); 	
+		// 	$http({
+		// 		method: 'GET',
+		// 		url: '/user/' + req.user.id, //or some sort of id ... 
+		// 		data: this
+		// 	}).then(function(response){
+		// 		console.log(response.data);
+		// 		console.log($scope);
+		// 	}, 
+		// 	function(err){
+		// 		console.log(err);
+		// 	});
+		// } else {
+		// alert('You are not signed up');
+		// }
 	};
 
 }]); //END Form Controller
